0137958a813f50324b24d4c7cd3314ff61bde5ed
Moritz Löwenstein <moritz.loewenstein@gmx.de>
2025-11-16 22:09:06 +0100

    maybe fix prisma db access in prod

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,15 +1,12 @@
 import { PrismaClient } from '../../../generated/prisma/client';
-import { PrismaBetterSQLite3 } from '@prisma/adapter-better-sqlite3';
-import { building } from '$app/environment';
 import { env } from '$env/dynamic/private';
 
-const dbUrl = building ? ':memory:' : env.DATABASE_URL;
-const adapter = new PrismaBetterSQLite3({
-	url: dbUrl
-});
 const prisma = new PrismaClient({
-	adapter,
-	log: ['error']
+	datasources: {
+		db: {
+			url: env.DATABASE_URL
+		}
+	}
 });
 
 async function getDbBackup(): Promise<Buffer | null> {
